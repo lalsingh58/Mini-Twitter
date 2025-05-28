4893393f17fa350017f4e78226a751cb8f14ab4b
Lal singh <lalsingh503511@gmail.com>
2025-05-28 18:48:28 +0530

    Created Mini twitter file and handles models and views

---
+++ tweet/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404, redirect
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def display_tweet(request):
+   Tweets =  Tweet.objects.all().order_by('-created_at')
+   return render(request, 'tweet_list.html', {'tweets':Tweets})
+
+def create_tweet(request):
+    if(request.method == "POST"):
+        form = TweetForm(request.POST,request.FILES)
+        if(form.is_valid()):
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('display_tweet')
+
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html',{'form':form})
+
+def edit_tweet(request, id):
+    tweet = get_object_or_404(Tweet,pk=id,user = request.user)
+    if(request.method == 'POST'):
+        form = TweetForm(request.POST,request.FILES,instance=tweet)
+        if(form.is_valid()):
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('display_tweet')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html',{'form':form})
+    
